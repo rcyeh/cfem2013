5d46063882a8a5f6d5d6bc76a0c19ac0829506da
jx226 <JiaXu@Jias-MacBook-Pro.local>
2013-09-12 12:16:45 -0400

    Add analisys on price prediciton using SIO
    R script in jia.R
    summary in Summary 1.docx

---
+++ RScripts/jia.R
@@ -0,0 +1,74 @@
+#install.packages("fBasics")
+#install.packages("car")
+library(fBasics)
+library(car)
+
+setwd("/Users/JiaXu/Documents/FE Project")
+source("parser.R")
+
+#Test SIO
+lm1 <-lm(SOI_buckets_delta_prices[,2]~SOI_buckets_delta_prices[,1])
+plot_model_stat(SOI_buckets_delta_prices[,2],SOI_buckets_delta_prices[,1])
+summary(lm1)
+
+lm5 <-lm(SOI_buckets_delta_prices[,2]~SOI_buckets_delta_prices[,1]+SOI_buckets_delta_prices[,1]*
+           SOI_buckets_delta_prices[,3])
+plot(SOI_buckets_delta_prices[,1]*
+      SOI_buckets_delta_prices[,3],SOI_buckets_delta_prices[,2])
+abline(lm(SOI_buckets_delta_prices[,2]~SOI_buckets_delta_prices[,3]),col="red")
+summary(lm5)
+residualPlots(lm5)
+
+ind = which(SOI_buckets_delta_prices[,2]==max(SOI_buckets_delta_prices[,2]))
+lm4 <-lm(SOI_buckets_delta_prices[-ind,2]~SOI_buckets_delta_prices[-ind,1]+SOI_buckets_delta_prices[-ind,1]*
+           SOI_buckets_delta_prices[-ind,3])
+summary(lm4)
+plot(SOI_buckets_delta_prices[-ind,1]*SOI_buckets_delta_prices[-ind,3],SOI_buckets_delta_prices[-ind,2])
+abline(lm(SOI_buckets_delta_prices[-ind,2]~SOI_buckets_delta_prices[-ind,3]),col="red")
+residualPlots(lm4)
+#0.09 R^2
+
+plot(SOI_buckets_delta_prices[,1]*
+       SOI_buckets_delta_prices[,3],SOI_buckets_delta_prices[,2],xlim=c(-5e-06,5e-06),ylim=c(-0.003,0.003))
+
+ind1 = which(SOI_buckets_delta_prices[,3]==0,arr.ind = T)
+length(which(SOI_buckets_delta_prices[,3]==0,arr.ind = T))
+thres = 0.3
+ind2 = which(abs(SOI_buckets_delta_prices[,1])<thres,arr.ind = T)
+ind3 = c(ind1,ind2)
+ind3
+xx = SOI_buckets_delta_prices[-ind3,1]*
+SOI_buckets_delta_prices[-ind3,3]
+yy = SOI_buckets_delta_prices[-ind3,2]
+plot.new()
+plot(xx,yy,xlim=c(-5e-06,5e-06),ylim=c(-0.003,0.003))
+
+lm3 <-lm(SOI_buckets_delta_prices[1:2000,2]~SOI_buckets_delta_prices[1:2000,1])
+plot_model_stat(SOI_buckets_delta_prices[1:2000,2],SOI_buckets_delta_prices[1:2000,1])
+summary(lm3)
+residualPlots(lm3)
+#.1255 R^2
+lm6 <-lm(SOI_buckets_delta_prices[1:2000,2]~SOI_buckets_delta_prices[1:2000,1]+SOI_buckets_delta_prices[1:2000,1]*SOI_buckets_delta_prices[1:2000,3])
+summary(lm6)
+
+########SIO prediction########
+lm7 <-lm(SOI_buckets_delta_prices[2:2001,2]~SOI_buckets_delta_prices[1:2000,1]+SOI_buckets_delta_prices[1:2000,1]*SOI_buckets_delta_prices[1:2000,3])
+summary(lm7)
+
+lm8 <-lm(SOI_buckets_delta_prices[2:2001,2]~SOI_buckets_delta_prices[1:2000,1]+
+           SOI_buckets_delta_prices[1:2000,1]*SOI_buckets_delta_prices[1:2000,3]+
+           SOI_buckets_delta_prices[1:2000,2])
+summary(lm8)
+
+#
+lm2 <- lm(SOI_buckets_delta_prices[which(SOI_buckets_delta_prices[,2]!=0,arr.ind = T),2]~
+            SOI_buckets_delta_prices[which(SOI_buckets_delta_prices[,2]!=0,arr.ind = T),1])
+summary(lm2)
+residualPlots(lm2)
+
+
+
+######### all the auotes and trades parsed
+SOI_buckets_delta_prices <- calc_OI_by_time_buckets(time_bin,a,bucket_size, F, L, T)
+
+SOI_buckets_delta_prices3 <- calc_OI_by_time_buckets(time_bin,a,bucket_size, F, L, T, T, T, T)
