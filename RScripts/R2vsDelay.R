f3822c3eb43a618794ea0c6dcb2376e17e3a27b5
jx226 <JiaXu@Jias-MacBook-Pro.local>
2013-10-18 15:02:31 -0400

    minor change to the plot format

--- RScripts/R2vsDelay.R
+++ RScripts/R2vsDelay.R
@@ -1,18 +1,17 @@
 library(aod)
 library(fBasics)
 library(car)
-
+library("ggplot2")
 setwd("/Users/JiaXu/Documents/FE project 2013/RScripts")
 source("parser.R")
 setwd("/Users/JiaXu/Documents/FE project")
-date = "20130430"
-tick = "AGN"
+date = "20130423"
+tick = "AMZN"
 #  "AGN"
-#TIF ,"BA", 
+#TIF ,"BA", "AMZN"
 a <- h5read(paste("ticks.",date,".h5",sep=""), paste("/ticks/",tick,sep=""), bit64conversion='double')
 
-delays <- c(30,100, 300, 1000, 3000)
-  ##seq(1,30,1)  c(seq(0,.5,.005), seq(1,10,1)) 
+delays <- c(seq(0,.5,.005), seq(1,10,1),c(1,5,10,30,100, 300, 1000, 3000))
 l_delays <- length(delays)
 thres_h = 1000
 r2s <- c()
@@ -20,7 +19,8 @@ for(i in 1:l_delays){
   delay = delays[i]
   delayed_a <- delay_quotes_xms(a,delay)
   trades_quotes <- filter_trades_quotes3(delayed_a, thres_h)
-  SOI_buckets_delta_prices <- calc_SOI(60, trades_quotes,F)
+  SOI_buckets_delta_prices1 <- calc_SOI(60, trades_quotes,F)
+  SOI_buckets_delta_prices <- SOI_buckets_delta_prices1[-c(1:5),]
 #exclude out of 2 standard deviation points from SOI, Preturn and Volatility
   means <- c()
   stdev2 <- c()
@@ -28,11 +28,19 @@ for(i in 1:l_delays){
     means[j] = mean(SOI_buckets_delta_prices[,j])
     stdev2[j] = 2*sd(SOI_buckets_delta_prices[,j])
   }
-  exind = which((abs(SOI_buckets_delta_prices[,1]-means[1])> stdev2[1]) | (abs(SOI_buckets_delta_prices[,2]-means[2]) > stdev2[2])) #| (abs(SOI_buckets_delta_prices[,3]-means[3])>stdev2[3]))
+  ind = which((abs(SOI_buckets_delta_prices[,1]-means[1])<= stdev2[1]) & (abs(SOI_buckets_delta_prices[,2]-means[2]) <= stdev2[2])) #| (abs(SOI_buckets_delta_prices[,3]-means[3])>stdev2[3]))
   #cross = SOI_buckets_delta_prices[,1]*sqrt(SOI_buckets_delta_prices[,3])
-  rdata = data.frame(PReturn = SOI_buckets_delta_prices[-exind,2], SOI = SOI_buckets_delta_prices[-exind,1])
-                     #cross = cross[-exind])
+  rdata = data.frame(PReturn = SOI_buckets_delta_prices[ind,2], SOI = SOI_buckets_delta_prices[ind,1])
+                     #cross = cross[ind])
   r2s[i] = summary(lm(PReturn ~ SOI,data = rdata))$adj.r.squared
-}
 
-plot(delays,r2s,type="l",main= paste(date," ", tick))
+}
+#plot(delays,r2s,type="l",main= paste("Current regression R^2 against quotes delay time\n",tick," on ",date,sep=""),ylab="R^2",xlab="delay (second)")
+ll = length(seq(0,.5,.005)) 
+#plot(delays[1:ll],r2s[1:ll],type="l",main= paste("Current regression R^2 against quotes delay time\n",tick," on ",date,sep=""),ylab="R^2",xlab="delay (second)")
+par(mfrow=c(1,2))
+qplot(delays,r2s,geom=c("line"),color="cyl",main= paste("Current regression R^2 against quotes delay time\n",tick," on ",date,sep=""),ylab="R^2",xlab="delay (second)")
+idd = which(delays[1:ll] == max(delays[1:ll]))
+qplot(delays[1:ll]*1000,r2s[1:ll],geom=c("line"),color="cyl",
+      main= paste("Zoom in: 1-500 millisecond part\n",tick," on ",date,sep=""),
+      ylab="R^2",xlab="delay (millisecond)")
