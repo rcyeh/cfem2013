655ca6928aaab5e9b0f6702f399cdc5f307aea9b
zc238 <zc238@cornell.edu>
2013-12-05 21:54:48 -0800

    Putting everything together for sub-proj1

---
+++ SubProject1_PriceImprovement/Analyzer.R
@@ -0,0 +1,25 @@
+plot_forward_vs_regress_bin = function(bin = 0.2, 
+                                       times <- c(180,120,90,60,30,10),
+                                       date='20130423'){
+  cols <- c('tan','chartreuse1','chartreuse4','cornflowerblue','blue')
+  #   cols <- rev(cols)
+  #   times <- c(60,50,40,30,20,10)
+  
+  excludeD <- excludeDs[ddd]
+  for (j in 1:length(times)){
+    time <- times[j]
+    file_name = paste('ExhaustiveAnalysis',date,'time',time,'_',useSOI,excludeD,'.csv',sep='')
+    sample <- read.csv(file=file_name,header=FALSE)
+    names(sample) <-c('Symbol','QOI','SOI','Forward','UForward')
+    N = round(2/bin)
+    bin = 2/N
+    bingroups <- aggregate(sample$Forward, by=list(floor(as.numeric(sample$SOI)/bin)), FUN = mean)
+    print(j)
+    if(j ==1){
+      plot(bingroups$Group.1*bin,bingroups$x,type='l',lwd = 2,col='yellow3',xlab ='SOI',ylab='return',main=paste('Return versus SOI pn 20130423',dep=''))
+    }else{
+      lines(bingroups$Group.1*bin,bingroups$x,type='l',lwd = 2,col=cols[j-1])
+    }
+  }
+  legend('topleft',legend = paste(times,'s',sep='') ,col = c('yellow3',cols),lwd = 2,text.font=4, ncol=4)
+}
